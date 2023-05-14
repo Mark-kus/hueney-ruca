a56ea21cb24b97330d1581eda1fc9dec9d18f922
Marco <tignanellimarco@gmail.com>
2023-05-13 22:50:15 -0300

    text alert fix

--- src/components/dashboard/swalError.js
+++ src/components/dashboard/swalError.js
@@ -3,7 +3,8 @@ import Swal from "sweetalert2";
 export default function swalError() {
     Swal.fire(
         "Ocurrió un error",
-        "No podimos cumplir con tu pedido\nIntentalo más tarde",
+        `No podimos cumplir con tu pedido,
+        intentalo más tarde`,
         "error"
     )
 }
