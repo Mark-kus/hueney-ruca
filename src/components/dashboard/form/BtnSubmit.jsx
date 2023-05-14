4584db22111d088a8efb01328a782bcda52b8217
Marco <tignanellimarco@gmail.com>
2023-05-14 19:59:38 -0300

    cancel button fix

--- src/components/dashboard/form/BtnSubmit.jsx
+++ src/components/dashboard/form/BtnSubmit.jsx
@@ -3,11 +3,12 @@ import Link from 'next/link'
 export default function (props) {
 	return (
 		<div className="flex justify-end gap-4.5">
-			<button
-				className="flex justify-center rounded border border-stroke py-2 px-6 font-medium text-black hover:shadow-1 dark:border-strokedark"
-				type="submit">
-				<Link href={ props.cancel_url }>Cancelar</Link>
-			</button>
+
+				<Link
+				 className="flex justify-center rounded border border-stroke py-2
+				 px-6 font-medium text-black hover:shadow-1 dark:border-strokedark" 
+				 href={ props.cancel_url }>Cancelar</Link>
+
 			<button
 				className="flex justify-center rounded bg-primary py-2 px-6 font-medium text-gray hover:bg-opacity-90"
 				type="submit"
