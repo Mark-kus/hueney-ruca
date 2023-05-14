faaf394276cbae4e0c0cb134be2bb2f33bddae59
Marco <tignanellimarco@gmail.com>
2023-05-14 17:14:00 -0300

    /admin route protected

--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -1,20 +1,39 @@
 import { useState, useEffect } from 'react'
 import Sidebar from '../components/dashboard/Sidebar'
 import Header from '../components/dashboard/Header'
+import { useSession } from '@supabase/auth-helpers-react';
+import { useRouter } from 'next/router';
+import { getProfileInfoId } from 'helpers/dbHelpers';
 
+export default function Layout({ children }) {
+	const [sidebarStatus, setSidebarStatus] = useState(true);
+	const [loading, setLoading] = useState(true);
+	const session = useSession();
+	const router = useRouter();
 
-export default function Layout({children}){
+	useEffect(() => {
+		async function getProfile() {
+			const user = await getProfileInfoId(session?.user?.id)
+			if (session === null || user.role < 2) {
+				router.push('/');
+			} else {
+				setLoading(false);
+			}
+		};
 
-	const [sidebarStatus, setSidebarStatus] = useState(true)
+		if (session) {
+			getProfile();
+		};
+	}, [session]);
 
 	const closeSidebar = () => setSidebarStatus(false)
 	const openSidebar = () => setSidebarStatus(true)
 
 	return (
 		<div className="relative z-1 bg-whiten font-satoshi text-base font-normal text-body">
-			{/* <Preload /> */}
-			<div className="flex h-screen overflow-hidden">
-				<Sidebar 
+			{/* <Preload />, loading, para que un usuario role 1 no llegue a ver nada */}
+			{loading ? null : <div className="flex h-screen overflow-hidden">
+				<Sidebar
 					sidebarStatus={sidebarStatus}
 					actionCloseSidebar={closeSidebar} />
 				<div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
@@ -25,7 +44,7 @@ export default function Layout({children}){
 						</div>
 					</main>
 				</div>
-			</div>
+			</div>}
 		</div>
 	)
 }
