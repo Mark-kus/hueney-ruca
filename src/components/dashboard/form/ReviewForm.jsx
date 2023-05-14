b6fe4a9bcf3113fa95e0878b8d7f1964d56b9088
Marco <tignanellimarco@gmail.com>
2023-05-13 23:50:51 -0300

    radio inputs now functional

--- src/components/dashboard/form/ReviewForm.jsx
+++ src/components/dashboard/form/ReviewForm.jsx
@@ -11,6 +11,8 @@ export default function ReviewForm({ review }) {
   const [status, setStatus] = useState(false);
   const [form, setForm] = useState({
     review: review?.review || "",
+    stars: review?.stars || 1,
+    approved: review?.approved || false,
   });
   const [errors, setErrors] = useState({});
 
@@ -28,14 +30,14 @@ export default function ReviewForm({ review }) {
 
   const getUserName = () => {
     const prof = profiles.filter((profile) => profile.id === review.user_id);
-    const userName = prof[0].full_name;
-    return userName;
+    return prof[0].full_name ? prof[0].full_name : '-';
   };
+
   const getUserEmail = () => {
     const prof = profiles.filter((profile) => profile.id === review.user_id);
-    const email = prof[0].email;
-    return email;
+    return prof[0].email ? prof[0].email : '-';
   };
+
   const handleChange = (e) => {
     let { name, value } = e.target;
 
@@ -54,12 +56,8 @@ export default function ReviewForm({ review }) {
         }
         break;
       case "approved":
-        if (value === "SI") {
-          value = true;
-        }
-        if (value === "NO") {
-          value = false;
-        }
+        if (value === 'true') value = Boolean(value);
+        else value = false;
         break;
 
       default:
@@ -142,21 +140,39 @@ export default function ReviewForm({ review }) {
 
           <div x-data="{ checkboxToggle: '' }" className="flex gap-x-5">
             {[...Array(5)].map((_, i) => (
-              <div key={i + 1} className="relative flex items-center">
+              <label
+                key={i}
+                className="cursor-pointer select-none relative 
+                inline-flex items-center gap-x-2"
+              >
                 <input
                   type="radio"
                   name="stars"
-                  id={i + 1}
                   value={i + 1}
-                  className="checked:bg-slate-500 h-5 w-5 mr-1 border cursor-pointer appearance-none rounded-full"
+                  className="hidden"
                   onChange={handleChange}
-                  required
                 />
 
-                <label htmlFor={i + 1} className="cursor-pointer select-none">
+                <span className={
+                  form.stars == i + 1
+                    ? `border border-primary h-5 w-5 
+                    grid place-content-center rounded-full 
+                    cursor-pointer 
+                    checked:bg-slate-400`
+                    : `border border-slate-400 h-5 w-5 
+                    grid place-content-center rounded-full 
+                    cursor-pointer 
+                    checked:bg-slate-500`
+                }
+                >
+                  {form.stars == i + 1 ? (
+                    <span className="bg-primary w-2.5 h-2.5 block rounded-full" />
+                  ) : null}
+                </span>
+                <span className="font-semibold">
                   {i + 1}
-                </label>
-              </div>
+                </span>
+              </label>
             ))}
           </div>
         </div>
@@ -189,24 +205,40 @@ export default function ReviewForm({ review }) {
           </label>
 
           <div x-data="{ checkboxToggle: '' }" className="flex gap-x-5">
-            {["SI", "NO"].map((approved, i) => (
-              <div key={i} className="relative flex items-center">
+            {[true, false].map((approved, i) => (
+              <label
+                key={i}
+                className="cursor-pointer select-none relative 
+                inline-flex items-center gap-x-2"
+              >
                 <input
                   type="radio"
                   name="approved"
-                  id={approved}
                   value={approved}
-                  className="checked:bg-slate-500 h-5 w-5 mr-1 border cursor-pointer appearance-none rounded-full"
+                  className="hidden"
                   onChange={handleChange}
-                  required
                 />
-                <label
-                  htmlFor={approved}
-                  className="cursor-pointer select-none"
+
+                <span className={
+                  form.approved == approved
+                    ? `border border-primary h-5 w-5 
+                    grid place-content-center rounded-full 
+                    cursor-pointer 
+                    checked:bg-slate-400`
+                    : `border border-slate-400 h-5 w-5 
+                    grid place-content-center rounded-full 
+                    cursor-pointer 
+                    checked:bg-slate-500`
+                }
                 >
-                  {approved}
-                </label>
-              </div>
+                  {form.approved == approved ? (
+                    <span className="bg-primary w-2.5 h-2.5 block rounded-full" />
+                  ) : null}
+                </span>
+                <span className="font-semibold">
+                  {approved === true ? 'SI' : 'NO'}
+                </span>
+              </label>
             ))}
           </div>
         </div>
