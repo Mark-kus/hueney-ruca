6cade8a2ca2909cc87312832e828b9019732b0e4
Marco <tignanellimarco@gmail.com>
2023-05-16 21:03:14 -0300

    responsive comentarios

--- src/pages/comentarios.jsx
+++ src/pages/comentarios.jsx
@@ -50,7 +50,7 @@ export default function Comentario() {
   return (
     <LayoutMain>
       <section className="h-screen px-4">
-        <div className="max-w-6xl mx-auto py-8">
+        <div className="max-w-6xl mx-auto md:py-8">
           <h2 className="text-brand-green text-4xl font-bold mb-6 mt-8">
             Nuestros huespedes!
           </h2>
